6d572f36960ee37ef586cd62aea34d247d1ff7e3
Josh Steverman <jstever@umich.edu>
2017-09-12 10:05:06 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# feddoc_oclc_nums
+
+A daily updated list of OCLC numbers determined to be Federal Documents. 
+
+https://www.hathitrust.org/usdocs_registry
---
+++ get_oclcs_from_registry.sh
@@ -0,0 +1,18 @@
+export PATH="$HOME/.rbenv/bin:$PATH"
+export PATH="$HOME/bin:$PATH"
+
+# export from the registry collection
+mongoexport -d htgd -c registry -q '{deprecated_timestamp:{$exists:0}}' -f oclcnum_t --csv -o oclcnums.tmp
+
+# split lines with multiple oclcs
+sed -i 's/,/\n/g' oclcnums.tmp
+
+# remove non-numbers
+sed -i 's/[^0-9]//g' oclcnums.tmp
+
+# sort / uniq them, remove empties and obvious stupidity
+sort oclcnums.tmp | uniq | grep -v '^$' | grep -vP '^[0-1]0?$' > feddoc_oclc_nums.txt
+
+git commit feddoc_oclc_nums.txt -m 'OCLC update'
+git commit --amend --author="Josh Steverman <jstever@umich.edu>"
+git push
