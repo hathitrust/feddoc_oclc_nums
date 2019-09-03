240a83444293ef1b6877d7a4c6917ffca4191131
Josh Steverman <jstever@umich.edu>
2019-09-03 10:47:29 -0400

    Get both lists

--- get_oclcs_from_registry.sh
+++ get_oclcs_from_registry.sh
@@ -13,5 +13,20 @@ sed -i 's/[^0-9]//g' oclcnums.tmp
 # sort / uniq them, remove empties and obvious stupidity
 sort oclcnums.tmp | uniq | grep -v '^$' | grep -vP '^[0-1]0?$' > feddoc_oclc_nums.txt
 
-git commit feddoc_oclc_nums.txt -m 'OCLC update' --author="Josh Steverman <jstever@umich.edu>"
+
+
+# and from the source files
+mongoexport --quiet -d htgd -c source_records -q '{deprecated_timestamp:{$exists:0}, in_registry:true}' -f oclc_alleged,oclc_resolved --csv -o oclcnums_raw.tmp
+
+# split lines
+sed -i 's/,/\n/g' oclcnums_raw.tmp
+sed -i 's/[]["]//g' oclcnums_raw.tmp
+
+# sort / uniq them, remove empties and obvious stupidity
+sort oclcnums_raw.tmp | uniq | grep -v '^$' | grep -v 'numberLong' | grep -v 'oclc_' | grep -v 'e' | grep -vP '^[0-1]0?$' > feddoc_oclc_nums_raw.txt
+
+
+
+
+git commit feddoc_oclc_nums.txt feddoc_oclc_nums_raw.txt -m 'OCLC update' --author="Josh Steverman <jstever@umich.edu>"
 git push
